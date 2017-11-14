08c31c998b3809625e8ec3e4a0fcf4a661ffa9ff
13696517880@163.com <13696517880@163.com>
2017-11-14 17:59:04 +0800

    Vue-seller初始化

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">商家</div>
+      <div class="tab-item">商品</div>
+    </div>
+    <div class="content">
+      I'm content
+    </div>
+
+  </div>
+</template>
+
+<script>
+import header from '@/components/header/Header.vue';
+export default {
+  components: {
+    'v-header': header
+  }
+};
+</script>
+
+<style type="text/stylus" lang="stylus" rel="stylesheet/stylus">
+#app
+  .tab
+    display: flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    .tab-item
+      flex: 1
+      text-align:center
+</style>
